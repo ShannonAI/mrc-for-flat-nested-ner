987410dbf7f9e3726c5439695c8f12a75c5fd2ea
mengyuxian <mengyuxian@shannonai.com>
2020-09-07 12:23:11 +0800

    FEA/init code

---
+++ models/bert_query_ner.py
@@ -0,0 +1,64 @@
+# encoding: utf-8
+"""
+@author: Yuxian Meng
+@contact: yuxian_meng@shannonai.com
+
+@version: 1.0
+@file: bert_query_ner
+@time: 2020/9/6 17:01
+@desc: 
+
+"""
+
+import torch
+import torch.nn as nn
+from transformers import BertModel, BertPreTrainedModel
+
+from models.classifier import MultiNonLinearClassifier
+
+
+class BertQueryNER(BertPreTrainedModel):
+    def __init__(self, config):
+        super(BertQueryNER, self).__init__(config)
+        self.bert = BertModel(config)
+
+        self.start_outputs = nn.Linear(config.hidden_size, 2)
+        self.end_outputs = nn.Linear(config.hidden_size, 2)
+        self.span_embedding = MultiNonLinearClassifier(config.hidden_size * 2, 1, config.mrc_dropout)
+
+        self.hidden_size = config.hidden_size
+
+        self.init_weights()
+
+    def forward(self, input_ids, token_type_ids=None, attention_mask=None):
+        """
+        Args:
+            input_ids: bert input tokens, tensor of shape [seq_len]
+            token_type_ids: 0 for query, 1 for context, tensor of shape [seq_len]
+            attention_mask: attention mask, tensor of shape [seq_len]
+        Returns:
+            start_logits: start/non-start probs of shape [seq_len, 2]
+            end_logits: end/non-end probs of shape [seq_len, 2]
+            match_logits: start-end-match probs of shape [seq_len, 1]
+        """
+
+        bert_outputs = self.bert(input_ids, token_type_ids, attention_mask)
+
+        sequence_heatmap = bert_outputs[0]  # [batch, seq_len, hidden]
+        batch_size, seq_len, hid_size = sequence_heatmap.size()
+
+        start_logits = self.start_outputs(sequence_heatmap)  # batch x seq_len x 2
+        end_logits = self.end_outputs(sequence_heatmap)  # batch x seq_len x 2
+
+        # for every position $i$ in sequence, should concate $j$ to
+        # predict if $i$ and $j$ are start_pos and end_pos for an entity.
+        # [batch, seq_len, seq_len, hidden]
+        start_extend = sequence_heatmap.unsqueeze(2).expand(-1, -1, seq_len, -1)
+        # [batch, seq_len, seq_len, hidden]
+        end_extend = sequence_heatmap.unsqueeze(1).expand(-1, seq_len, -1, -1)
+        # [batch, seq_len, seq_len, hidden*2]
+        span_matrix = torch.cat([start_extend, end_extend], 3)
+        # [batch, seq_len, seq_len]
+        span_logits = self.span_embedding(span_matrix).squeeze(-1)
+
+        return start_logits, end_logits, span_logits
---
+++ models/classifier.py
@@ -0,0 +1,42 @@
+# encoding: utf-8
+"""
+@author: Yuxian Meng
+@contact: yuxian_meng@shannonai.com
+
+@version: 1.0
+@file: classifier
+@time: 2020/9/6 17:02
+@desc: 
+
+"""
+
+
+import torch.nn as nn
+
+
+class SingleLinearClassifier(nn.Module):
+    def __init__(self, hidden_size, num_label):
+        super(SingleLinearClassifier, self).__init__()
+        self.num_label = num_label
+        self.classifier = nn.Linear(hidden_size, num_label)
+
+    def forward(self, input_features):
+        features_output = self.classifier(input_features)
+
+        return features_output
+
+
+class MultiNonLinearClassifier(nn.Module):
+    def __init__(self, hidden_size, num_label, dropout_rate):
+        super(MultiNonLinearClassifier, self).__init__()
+        self.num_label = num_label
+        self.classifier1 = nn.Linear(hidden_size, int(hidden_size / 2))
+        self.classifier2 = nn.Linear(int(hidden_size/2), num_label)
+        self.dropout = nn.Dropout(dropout_rate)
+
+    def forward(self, input_features):
+        features_output1 = self.classifier1(input_features)
+        features_output1 = nn.ReLU()(features_output1)
+        features_output1 = self.dropout(features_output1)
+        features_output2 = self.classifier2(features_output1)
+        return features_output2
