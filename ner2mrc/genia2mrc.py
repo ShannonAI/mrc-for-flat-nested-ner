fc0f019151ba817b4e3f623a46e272b386458de7
mengyuxian <mengyuxian@shannonai.com>
2020-09-12 22:02:06 +0800

    FEA/ace0405 sh and genia dataset

---
+++ ner2mrc/genia2mrc.py
@@ -0,0 +1,61 @@
+# encoding: utf-8
+"""
+@author: Yuxian Meng
+@contact: yuxian_meng@shannonai.com
+
+@version: 1.0
+@file: genia2mrc.py
+@time: 2020/9/12 20:19
+@desc: 
+
+"""
+
+
+import os
+from utils.bmes_decode import bmes_decode
+import json
+
+
+def convert_file(input_file, output_file, tag2query_file):
+    """
+    Convert GENIA(xiaoya) data to MRC format
+    """
+    all_data = json.load(open(input_file))
+    tag2query = json.load(open(tag2query_file))
+
+    output = []
+    origin_count = 0
+    new_count = 0
+
+    for data in all_data:
+        origin_count += 1
+        context = data["context"]
+        label2positions = data["label"]
+        for tag, query in tag2query.items():
+            positions = label2positions.get(tag, [])
+            mrc_sample = {
+                "context": context,
+                "query": query,
+                "start_position": [int(x.split(";")[0]) for x in positions],
+                "end_position": [int(x.split(";")[1]) for x in positions]
+            }
+            output.append(mrc_sample)
+            new_count += 1
+
+    json.dump(output, open(output_file, "w"), ensure_ascii=False, indent=2)
+    print(f"Convert {origin_count} samples to {new_count} samples and save to {output_file}")
+
+
+def main():
+    genia_raw_dir = "/mnt/mrc/genia/genia_raw"
+    genia_mrc_dir = "/mnt/mrc/genia/genia_raw/mrc_format"
+    tag2query_file = "queries/genia.json"
+    os.makedirs(genia_mrc_dir, exist_ok=True)
+    for phase in ["train", "dev", "test"]:
+        old_file = os.path.join(genia_raw_dir, f"{phase}.genia.json")
+        new_file = os.path.join(genia_mrc_dir, f"mrc-ner.{phase}")
+        convert_file(old_file, new_file, tag2query_file)
+
+
+if __name__ == '__main__':
+    main()
--- scripts/reproduce/ace05.sh
+++ scripts/reproduce/ace05.sh
@@ -1,28 +1,31 @@
-DATA_DIR="/mnt/mrc/ace2004"
+export PYTHONPATH="$PWD"
+# 在epoch=15时F1达到86.1
+DATA_DIR="/mnt/mrc/ace2005"
 BERT_DIR="/mnt/mrc/bert-large-uncased"
 
 BERT_DROPOUT=0.1
-MRC_DROPOUT=0.3
+MRC_DROPOUT=0.5
 LR=3e-5
 SPAN_WEIGHT=0.1
 WARMUP=0
 MAXLEN=128
 MAXNORM=1.0
 
-OUTPUT_DIR="/mnt/mrc/train_logs/ace2004/ace2004_20200910reproduce_lr${LR}_drop${MRC_DROPOUT}_norm${MAXNORM}_bsz32_hard_span_weight${SPAN_WEIGHT}_warmup${WARMUP}_maxlen${MAXLEN}_newtrunc_debug"
+OUTPUT_DIR="/mnt/mrc/train_logs/ace2005/ace2005_20200911_lr${LR}_drop${MRC_DROPOUT}_norm${MAXNORM}_bsz32_hard_span_weight${SPAN_WEIGHT}_warmup${WARMUP}_maxlen${MAXLEN}"
 mkdir -p $OUTPUT_DIR
+
 python trainer.py \
 --data_dir $DATA_DIR \
 --bert_config_dir $BERT_DIR \
 --max_length $MAXLEN \
---batch_size 4 \
---gpus="2,3" \
+--batch_size 16 \
+--gpus="0,1" \
 --precision=16 \
 --progress_bar_refresh_rate 1 \
 --lr $LR \
 --distributed_backend=ddp \
 --val_check_interval 0.5 \
---accumulate_grad_batches 4 \
+--accumulate_grad_batches 1 \
 --default_root_dir $OUTPUT_DIR \
 --mrc_dropout $MRC_DROPOUT \
 --bert_dropout $BERT_DROPOUT \
